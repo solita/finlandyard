92ac3e07bf02a4e467375b83b00006bb1c17e7c1
Juho Friman <juho.t.friman@gmail.com>
2016-11-21 20:21:00 +0200

    Clock ticks again in Finland Yard

--- src/app.js
+++ src/app.js
@@ -2,12 +2,30 @@ require('file?name=[name].[ext]!./index.html');
 var stateUtils = require('./StateUtils.js');
 var mapControl = require('./MapControl.js');
 var loadData = require('./Api.js');
+var moment = require('moment')
 
 console.log("Starting up finland yard");
 
 var mapControl = mapControl();
 
 loadData(function(state) {
+  var startingTime = moment(state.timetable[0].timeTableRows[0].scheduledTime);
+  state.clockIs = startingTime.clone();
   mapControl.drawConnections(stateUtils.collectConnections(state));
   mapControl.drawStations(stateUtils.connectedStations(state));
+
+  // THE game loop
+  (function tick() {
+    setTimeout(
+      function() {
+        // Edistä kelloa
+        state.clockIs = state.clockIs.add(1, 'minutes');
+        if(state.clockIs.unix() - startingTime.unix() > 1 * 24 * 60 * 60) {
+          state.clockIs = startingTime.clone();
+        }
+        document.getElementById("clock").innerHTML = state.clockIs.format();
+
+        tick();
+      },
+      10)})();
 });
