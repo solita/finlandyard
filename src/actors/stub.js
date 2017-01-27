3db5ed0424b76350eddcd870c0ca9313d6c579ad
koodiritari <koodiritari>
2017-01-27 15:04:03 +0200

    Stubbin

--- src/actors/stub.js
+++ src/actors/stub.js
@@ -5,15 +5,22 @@ var Actions = require('../Actions.js');
 
 // Utils
 /*
-  R.first(array)
+  R.head(array)
   R.last(array)
   .. and so on
 */
+
+// Utility for returning random entry from array  randomNth([1, 2, 3]) -> 2
+var randomNth = (coll) =>  R.nth(Math.floor(Math.random() * coll.length), coll);
 
 ActorBridge.registerActor('police', 'stub', 'JNS', function(clockIs, context, actor) {
   console.log(context);
   // Get trains leaving from station
   var trainsLeavingArray = dataUtils.trainsLeavingFrom(clockIs, actor.location);
+  // Take first train
+  var train = R.head(trainsLeavingArray);
+  // Take last station from train
+  var hopOffStation = R.last(dataUtils.getPossibleHoppingOffStations(train, actor.location));
   // Hop to first train and got to the last station
-  return Actions.train(u[0], R.last(dataUtils.getPossibleHoppingOffStations(u[0], actor.location)))
+  return Actions.train(train, hopOffStation);
 });
