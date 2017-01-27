5b4a4a332e286a1a4806fe8d8698e2f5ec4e0d1e
koodiritari <koodiritari>
2017-01-27 18:02:12 +0200

    Solved

--- src/actors/stub.js
+++ src/actors/stub.js
@@ -2,6 +2,7 @@ var ActorBridge = require('../ActorBridge.js');
 var dataUtils = require('../state/DataUtils.js');
 var R = require('ramda');
 var Actions = require('../Actions.js');
+var _ = require('lodash');
 
 // Utils
 /*
@@ -13,14 +14,29 @@ var Actions = require('../Actions.js');
 // Utility for returning random entry from array  randomNth([1, 2, 3]) -> 2
 var randomNth = (coll) =>  R.nth(Math.floor(Math.random() * coll.length), coll);
 
-ActorBridge.registerActor('police', 'stub', 'JNS', function(clockIs, context, actor) {
+ActorBridge.registerActor('police', 'sauna-lovers', 'JNS', function(clockIs, context, actor) {
   console.log(context);
+
+  var villainLocation = R.last(context.knownVillainLocations);
   // Get trains leaving from station
   var trainsLeavingArray = dataUtils.trainsLeavingFrom(clockIs, actor.location);
+  var ctrain;
+  ctrain = chooseTrain(villainLocation);
+  if (!ctrain) {
+    villainLocation = 'HKI'
+    ctrain = chooseTrain('HKI');
+  }
+  function chooseTrain(loc) {
+  for (var train of trainsLeavingArray) {
+    var possibleDestinationsArray = dataUtils.getPossibleHoppingOffStations(train, actor.location);
+    if (_.includes(possibleDestinationsArray, loc)) {
+      return train;
+      break;
+    }
+  }
+  }
   // Take first train
-  var train = R.head(trainsLeavingArray);
   // Take last station from train
-  var hopOffStation = R.last(dataUtils.getPossibleHoppingOffStations(train, actor.location));
   // Hop to first train and got to the last station
-  return Actions.train(train, hopOffStation);
+  return Actions.train(ctrain, villainLocation);
 });
