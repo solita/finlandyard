1337e140bc7a462e03098e47340ba2e6e544d0e8
koodiritari <koodiritari>
2017-01-27 14:55:27 +0200

    stubbing

--- src/actors/stub.js
+++ src/actors/stub.js
@@ -3,8 +3,17 @@ var dataUtils = require('../state/DataUtils.js');
 var R = require('ramda');
 var Actions = require('../Actions.js');
 
-
+// Utils
+/*
+  R.first(array)
+  R.last(array)
+  .. and so on
+*/
 
 ActorBridge.registerActor('police', 'stub', 'JNS', function(clockIs, context, actor) {
-  return Actions.idle();
+  console.log(context);
+  // Get trains leaving from station
+  var trainsLeavingArray = dataUtils.trainsLeavingFrom(clockIs, actor.location);
+  // Hop to first train and got to the last station
+  return Actions.train(u[0], R.last(dataUtils.getPossibleHoppingOffStations(u[0], actor.location)))
 });
