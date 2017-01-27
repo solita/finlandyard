d83a548cafe4b45fd12e141ce79b4e2b7167985b
koodiritari <koodiritari>
2017-01-27 19:43:17 +0200

    solved

--- src/actors/stub.js
+++ src/actors/stub.js
@@ -13,14 +13,26 @@ var Actions = require('../Actions.js');
 // Utility for returning random entry from array  randomNth([1, 2, 3]) -> 2
 var randomNth = (coll) =>  R.nth(Math.floor(Math.random() * coll.length), coll);
 
-ActorBridge.registerActor('police', 'stub', 'JNS', function(clockIs, context, actor) {
+ActorBridge.registerActor('police', 'betteryellow', 'JNS', function(clockIs, context, actor) {
   console.log(context);
   // Get trains leaving from station
   var trainsLeavingArray = dataUtils.trainsLeavingFrom(clockIs, actor.location);
-  // Take first train
-  var train = R.head(trainsLeavingArray);
-  // Take last station from train
-  var hopOffStation = R.last(dataUtils.getPossibleHoppingOffStations(train, actor.location));
-  // Hop to first train and got to the last station
-  return Actions.train(train, hopOffStation);
+  var villainLocation = context.knownVillainLocations;
+  for (var i  = 0; i < trainsLeavingArray.length; i++) {
+    var train = trainsLeavingArray[i];
+    var stations = dataUtils.getPossibleHoppingOffStations(train, actor.location);
+    for (var j = 0; j < stations.length; j++) {
+      var station = stations[j];
+      for (var k = 0; k < villainLocation.length; k++) {
+        var vilLoc = villainLocation[k];
+        if (station == vilLoc) {
+          console.log(train, station);
+          return Actions.train(train, station);
+        }
+      }
+    }
+  }
+  var train = train(randomNth(trainsLeavingFrom));
+  var loc = R.head(dataUtils.getPossibleHoppingOffStations(train, actor.location))
+  return Actions.train(train, loc)
 });
