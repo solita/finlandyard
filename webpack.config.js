32ed79fa0e13bd1a4571fc46cc81516f1243bb89
Juhani Kumara <juhani.kumara@solita.fi>
2018-01-24 18:29:56 +0200

    Added babel configuration for transpiling ES6+ to old syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
-
 module.exports = {
-    entry: './src/FinlandYard.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: "bundle.js"
-    },
-    module: {
-      rules: [
-            {
-              test: /\.css$/,
-              use: [ 'style-loader', 'css-loader' ]
-            }
-          ]
-    },
-    devtool: 'source-map'
+  entry: './src/FinlandYard.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'source-map'
 };
