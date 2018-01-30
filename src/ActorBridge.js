61ccfaf4c45fdb5fe7576ea076075055a3933878
Anniina <anniina.sallinen@cs.helsinki.fi>
2018-01-30 17:37:29 +0200

    Changed Mehler to be random as well

--- src/ActorBridge.js
+++ src/ActorBridge.js
@@ -4,7 +4,7 @@ const actors = [
       {type: 'villain', name: 'Badmouth', location: 'HKI', caught: false, freeMinutes: 0, aifn: AI.random },
       {type: 'villain', name: 'Mongoose', location: 'TPE', caught: false, freeMinutes: 0, aifn: AI.random },
       {type: 'villain', name: 'Luke', location: 'LR', caught: false, freeMinutes: 0, aifn: AI.random },
-      {type: 'villain', name: 'Mehler', location: 'VS', caught: false, freeMinutes: 0, aifn: AI.noop },
+      {type: 'villain', name: 'Mehler', location: 'VS', caught: false, freeMinutes: 0, aifn: AI.random },
       {type: 'villain', name: 'Stiff Little Finger', location: 'TPE', caught: false, freeMinutes: 0, aifn: AI.random },
 ];
 
