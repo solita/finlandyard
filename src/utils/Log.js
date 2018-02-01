8323373a0f65061956869b6a0e7219c5b83c247b
Juhani Kumara <juhani.kumara@solita.fi>
2018-02-01 15:50:35 +0200

    Add logging utility

--- src/utils/Log.js
+++ src/utils/Log.js
@@ -1,6 +1,17 @@
+import R from 'ramda'
 
-module.exports = {
-  log: function(time, msg) {
-    console.log(msg);
-  }
-};
+const logElement = document.querySelector('.js-log');
+const logElementContainer = document.querySelector('.js-log');
+
+const logToHTML = msg => {
+  const paragraph = document.createElement('p');
+  paragraph.innerText = msg;
+  logElement.appendChild(paragraph);
+  logElement.scrollTop = logElement.scrollHeight;
+  return msg;
+}
+
+export const log = R.pipe(
+  R.tap(console.log),
+  logToHTML
+)
