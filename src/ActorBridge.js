fcaa9c0de95f92ec33fa39ecc9915cf7228e6c2c
Juho Friman <juho.t.friman@gmail.com>
2018-02-02 13:47:48 +0200

    Added one villain to master

--- src/ActorBridge.js
+++ src/ActorBridge.js
@@ -2,7 +2,7 @@ import AI from './AI.js'
 
 // Propert isPlayer: true, marks the player bot
 const actors = [
-  //{type: 'villain', name: 'Badmouth', location: 'OL', caught: false, freeMinutes: 0,money: 0, aifn: AI.crime},
+  {type: 'villain', name: 'Badmouth', location: 'OL', caught: false, freeMinutes: 0,money: 0, aifn: AI.crime},
   //{type: 'villain', name: 'Mongoose', location: 'TPE', caught: false, freeMinutes: 0,money: 0, aifn: AI.crime },
   //{type: 'villain', name: 'Luke', location: 'LR', caught: false, freeMinutes: 0,money: 0, aifn: AI.random },
   //{type: 'villain', name: 'Mehler', location: 'VS', caught: false, freeMinutes: 0,money: 0, aifn: AI.random },
